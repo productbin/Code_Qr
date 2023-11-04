b55fe653249cf8a29419dfd8e15b75d0fa125f2b
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-11-04 19:31:23 +0530

    fix(qr):url save

--- script.js
+++ script.js
@@ -2,42 +2,46 @@ document.addEventListener("DOMContentLoaded", function () {
   const generateButton = document.getElementById("generate-qr");
   const qrDataInput = document.getElementById("qr-data");
   const saveButton = document.getElementById("save-qr");
-  const qrcode = new QRCode(document.getElementById("qrcode"), {
-    width: 300,
-    height: 300,
-  });
 
   generateButton.addEventListener("click", function () {
     const data = qrDataInput.value;
     if (data) {
-      qrcode.makeCode(data);
+      // Clear the previous QR code if it exists
+      const previousQRCode = document.querySelector("#qrcode img");
+      if (previousQRCode) {
+        previousQRCode.remove();
+      }
+
+      // Create a new QR code image
+      const qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: data,
+        width: 300,
+        height: 300,
+      });
+
+      saveButton.disabled = false; // Enable the "Save" button when a QR code is generated
+    } else {
+      saveButton.disabled = true; // Disable the "Save" button when there's no data
     }
   });
 
   saveButton.addEventListener("click", function () {
-    const data = qrDataInput.value;
-    if (data) {
-      const qrCodeImage = document.querySelector("#qrcode img");
-      if (qrCodeImage) {
-        const canvas = document.createElement("canvas");
-        const context = canvas.getContext("2d");
-
-        canvas.width = qrCodeImage.width;
-        canvas.height = qrCodeImage.height;
-        context.drawImage(qrCodeImage, 0, 0);
-
-        // Convert the QR code image to a data URL
-        const dataURL = canvas.toDataURL("image/png");
-
-        // Create a download link for the QR code image
-        const a = document.createElement("a");
-        a.href = dataURL;
-        a.download = "qrcode.png"; // You can set the filename as you like
-        a.style.display = "none";
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-      }
+    const qrCodeImage = document.querySelector("#qrcode img");
+    if (!qrCodeImage) {
+      // Check if the QR code is empty or not generated
+      alert("Please generate a QR code before saving.");
+      return;
     }
+
+    const dataURL = qrCodeImage.src;
+
+    // Create a download link for the QR code image
+    const a = document.createElement("a");
+    a.href = dataURL;
+    a.download = "qrcode.png"; // You can set the filename as you like
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   });
 });
