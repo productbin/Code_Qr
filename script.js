6a5c98c0a05a78e00d2ff47a5ad7561136625427
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-11-04 18:56:52 +0530

    feat(qr):save qr

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const generateButton = document.getElementById("generate-qr");
   const qrDataInput = document.getElementById("qr-data");
+  const saveButton = document.getElementById("save-qr");
   const qrcode = new QRCode(document.getElementById("qrcode"), {
     width: 300,
     height: 300,
@@ -10,6 +11,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const data = qrDataInput.value;
     if (data) {
       qrcode.makeCode(data);
+    }
+  });
+
+  saveButton.addEventListener("click", function () {
+    const data = qrDataInput.value;
+    if (data) {
+      const qrCodeImage = document.querySelector("#qrcode img");
+      if (qrCodeImage) {
+        // Create a download link for the QR code image
+        const a = document.createElement("a");
+        a.href = qrCodeImage.src;
+        a.download = "qrcode.png"; // You can set the filename as you like
+        a.style.display = "none";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
     }
   });
 });
