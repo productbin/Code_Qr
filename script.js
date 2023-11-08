f968cbc0317abc5fab6899a795483980588275c7
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-11-08 12:05:16 +0530

    feat(qr):refactor method

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+// JavaScript Code
 const generateButton = document.getElementById("generate-button");
 const downloadButton = document.getElementById("download-button");
+const inputField = document.getElementById("text-input");
+const inputWarning = document.getElementById("input-warning");
+
 const qrCode = new QRCode(document.getElementById("qr-code"), {
   width: 300,
   height: 300,
 });
 
 generateButton.addEventListener("click", () => {
-  const textInput = document.getElementById("text-input").value;
+  const textInput = inputField.value;
+
+  if (textInput.length < 15) {
+    // Minimum length validation
+    inputWarning.textContent = "Invalid input"; // Display warning
+    return;
+  } else {
+    inputWarning.textContent = ""; // Clear any previous warning
+  }
+
   qrCode.clear();
   qrCode.makeCode(textInput);
   downloadButton.style.display = "block";
